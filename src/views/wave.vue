974abccd2d8501a26d272d7f6f1194e1f3726e6e
kongjianqiu <872221317@qq.com>
2022-10-31 18:31:59 +0800

    commit

---
+++ src/views/wave.vue
@@ -0,0 +1,29 @@
+<!--
+threejs wave.vue
+@author kongjianqiu
+@description
+@created 2022/10/31 18:06:11
+-->
+<template>
+  <div class="wave" id="wave"></div>
+</template>
+
+<script>
+// threeJs插件
+import * as THREE from "three";
+export default {
+  name: "wave",
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {
+  },
+  created() {
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
