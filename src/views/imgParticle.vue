e5311bcfbc652ded0822e400bcc88b64be9d7005
kongjianqiu <872221317@qq.com>
2022-11-02 18:35:33 +0800

    commit

--- src/views/imgParticle.vue
+++ src/views/imgParticle.vue
@@ -6,7 +6,7 @@ threejs imgParticle.vue
 -->
 <template>
   <div id="imgParticle" class="imgParticle">
-    <canvas id="canvas2"></canvas>
+
   </div>
 </template>
 
@@ -15,7 +15,7 @@ threejs imgParticle.vue
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-let img, canvas, ctx, width, height, imgData, numPixels, originalColors, numVisible = 0, threshold = 100, offsets;
+let img, canvas, ctx, imgData, particles, size = 3, cw, ch, points, loaded;
 let scene, group, camera, renderer, controls;
 export default {
   name: "imgParticle",
@@ -27,100 +27,64 @@ export default {
     };
   },
   methods: {
+    initImg(imgUrl) {
+      canvas = document.createElement("canvas");
+      ctx = canvas.getContext("2d");
+      img = new Image();
+      img.src = imgUrl;
+      img.onload = () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.width);
+        imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        this.getParticleData();
+        this.initThree();
+        this.animate();
+      };
+    },
     getParticleData() {
-      // 统计大于阈值的像素点
-      for (let i = 0; i < numPixels; i++) {
-        if (originalColors[i * 4 + 0] > threshold) numVisible++;
+      if (points) {
+        scene.remove(points);
       }
-      offsets = new Float32Array(numVisible * 3);
-      // 获取像素点的位置
-      for (let i = 0, j = 0; i < numPixels; i++) {
-        if (originalColors[i * 4 + 0] > threshold) {
-          // 获取 x 方向的坐标
-          offsets[j * 3 + 0] = i % width;
-          // 获取 y 方向的坐标
-          offsets[j * 3 + 1] = Math.floor(i / width);
-          j++;
-        }
-      }
+      cw = Math.floor(canvas.width / size);
+      ch = Math.floor(canvas.height / size);
+      particles = cw * ch;
     },
     initThree() {
       scene = new THREE.Scene();
-      camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-      camera.position.z = 300;
-      let fovHeight = 2 * Math.tan(camera.fov * Math.PI / 180 / 2) * camera.position.z;
-      let canvas2 = document.getElementById("canvas2")
-      canvas2.width = window.innerWidth
-      canvas2.height = window.innerHeight
-      renderer = new THREE.WebGLRenderer({
-        canvas: canvas2,
-        antialias: true,
-        alpha: true
-      });
-      renderer.setClearColor(0x000000, 1);
+      camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 3000);
+      camera.position.z = 500;
+      renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+      renderer.setClearAlpha(0);
+      renderer.setSize(window.innerWidth, window.innerHeight);
       controls = new OrbitControls(camera, renderer.domElement);
-    },
-    initMesh() {
-      group = new THREE.Group();
-      let textureLoader = new THREE.TextureLoader();
-      let map = textureLoader.load(this.imgUrl2);
-      let material = new THREE.SpriteMaterial({
-        map,
-        color: 0xffffff,
-        fog: true
-      });
-      let positions = offsets;
-      for (let index = 0; index < positions.length; index += 2) {
-        let particleMaterial = material;
-        let particle = new THREE.Sprite(particleMaterial);
-        // 粒子目标位置
-        let targetX = positions[index];
-        let targetY = positions[index + 1];
-        let targetZ = positions[index + 2];
-
-        if (targetX && targetY) {
-          particle.position.x = targetX;
-          particle.position.y = targetY;
-          particle.position.z = targetZ;
-          /* // 粒子的初始位置
-           particle.position.x = 0;
-           particle.position.y = 0;
-           particle.position.z = 0;
-           // 粒子从初始位置过渡到目标位置
-           TweenMax.to(particle.position, 1, {
-             x: targetX,
-             y: targetY,
-             z: targetZ,
-             delay: Math.random() * 0.1
-           });*/
-          group.add(particle);
-        }
+      let dom = document.getElementById("imgParticle");
+      dom.appendChild(renderer.domElement);
+      let geometry = new THREE.BufferGeometry();
+      let positions = new Float32Array(Math.floor(particles * 3));
+      // let positions_af = new Float32Array(Math.floor(particles * 3));
+      let colors = new Float32Array(Math.floor(particles * 3));
+      for (let i = 0; i < positions.length; i += 1) {
+        positions[3 * i] = -canvas.width / 2 + (i % cw) * size;
+        positions[3 * i + 1] = canvas.height / 2 + Math.floor((-1 - i) / cw) * size;
+        positions[3 * i + 2] = 0;
+        let selectPos = size * (i % cw) + Math.floor(i / cw) * cw * size * size;
+        colors[3 * i] = imgData.data[4 * selectPos] / 255.0;
+        colors[3 * i + 1] = imgData.data[4 * selectPos + 1] / 255.0;
+        colors[3 * i + 2] = imgData.data[4 * selectPos + 2] / 255.0;
       }
-      scene.add(group);
-    },
-    initImg(imgUrl) {
-      img = document.createElement("img");
-      img.src = imgUrl;
-      img.onload = () => {
-        canvas = document.createElement("canvas");
-        ctx = canvas.getContext("2d");
-        width = img.width;
-        height = img.height;
-        numPixels = width * height;
-        canvas.width = width;
-        canvas.height = height;
-        ctx.scale(1, -1);
-        ctx.drawImage(img, 0, 0, width, height * -1);
-        imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        originalColors = Float32Array.from(imgData.data);
-        this.getParticleData();
-        this.initThree();
-        this.initMesh();
-        this.animate()
-      };
+      geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+      geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+      geometry.dynamic = true;
+      geometry.attributes.position.needsUpdate = true;
+      let material = new THREE.PointsMaterial({ size: size, vertexColors: true });
+      points = new THREE.Points(geometry, material);
+      points.name = "points";
+      scene.add(points);
+      loaded = true;
     },
     animate() {
-      controls.update()
+      controls.update();
       renderer.render(scene, camera);
       requestAnimationFrame(this.animate);
     }
